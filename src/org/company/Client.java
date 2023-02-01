945c8cf8a19bbce90bfa46dd9f13fec5315896cc
keerthiprithiv <keerthisin1998@gmail.com>
2023-02-01 22:33:19 +0530

    testone

---
+++ src/org/company/Client.java
@@ -0,0 +1,12 @@
+package org.company;
+
+public class Client {
+public void clientName() { System.out.println();}
+public static void main(String[] args) {
+	
+	
+	
+	
+}
+
+}
---
+++ src/org/company/Company.java
@@ -0,0 +1,12 @@
+package org.company;
+
+public class Company extends Client {
+public void companyName() { System.out.println();}
+public static void main(String[] args) {
+	Company candidateDetails=new Company();
+	candidateDetails.companyName();
+	candidateDetails.clientName();
+	
+	
+}
+}
